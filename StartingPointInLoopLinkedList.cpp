7bf749ccb0d7b791bbaa42be3b6bfc93c0487607
Mann Shah <107050272+pythonman1234@users.noreply.github.com>
2025-04-30 11:57:47 +0530

    Create StartingPointInLoopLinkedList.cpp

---
+++ StartingPointInLoopLinkedList.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if(fast==slow){
+                slow = head;
+                while(slow!=fast){
+
+                
+                slow = slow->next;
+                fast = fast->next;
+                }
+                return slow;
+            }
+            
+        }
+        return nullptr;
+    }
+};
