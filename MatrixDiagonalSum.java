330186f6ffda2ec17cc123e319ed77517529bcf9
Mann Shah <107050272+pythonman1234@users.noreply.github.com>
2025-05-18 09:56:29 +0530

    Create MatrixDiagonalSum.java

---
+++ MatrixDiagonalSum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int diagonalSum(int[][] matrix) {
+        int sum = 0;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(i==j){
+                    sum+=matrix[i][j];
+                }
+                else if(i+j==matrix.length-1){
+                    sum+=matrix[i][j];
+                }
+            }
+        }
+        return sum;
+   
