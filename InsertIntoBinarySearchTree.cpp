23fa90df334a060b2e46dbe8eff99d16c310039e
Mann Shah <107050272+pythonman1234@users.noreply.github.com>
2025-07-28 05:23:34 +0530

    Create InsertIntoBinarySearchTree.cpp

---
+++ InsertIntoBinarySearchTree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL){
+            TreeNode* newNode = new TreeNode(val);
+            return newNode;
+        }
+        if(val<root->val){
+            root->left = insertIntoBST(root->left,val);
+        }
+        else{
+            root->right = insertIntoBST(root->right,val);
+        }
+        return root;
+    }
+};
